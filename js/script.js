17db9436dd1caec94de09377b2c6274103f5842a
abzhikai <abzhikai@gmail.com>
2022-08-14 13:57:48 +0800

    Rebuild

--- js/script.js
+++ js/script.js
@@ -1,25 +1,7 @@
-/* Open the drawer when the menu icon is clicked. */
-var menu = document.querySelector('.menu');
-var main = document.querySelector('.main');
-var drawer = document.querySelector('.drawer');
-
-// menu.addEventListener('click', function(e)
-// {
-//     for(let i = 0; i < drawer.length;i++)
-//     {
-//         drawer.classList.toggle('open');
-//         e.stopPropagation();
-//     }
-// });
-// main.addEventListener('click', function()
-// {
-//     drawer.classList.remove('open');
-// });
-
 var navbutton = document.querySelector('.nav-button-container');
 var showsubtopics = document.querySelector('.subtopics');
 
-navbutton.addEventListener('click', function(e)
+navbutton.addEventListener('click', function()
 {
     if(showsubtopics.style.display == "none") 
     {
@@ -31,15 +13,86 @@ navbutton.addEventListener('click', function(e)
     }
 });
 
-var mangabutton = document.querySelector('.nav-button-container');
+var opmmangabutton = document.querySelector('#opm');
+var kbmangabutton = document.querySelector('#kb');
+var sdmangabutton = document.querySelector('#sd');
 
-// var opm = document.querySelector('')
+var showpanels1 = document.querySelector('.opm-items')
+var showpanels2 = document.querySelector('.kb-items')
+var showpanels3 = document.querySelector('.sd-items')
 
-// menu.addEventListener('click', function(e)
-// {
-//     for(let i = 0; i < drawer.length;i++)
-//     {
-//         drawer.classList.toggle('open');
-//         e.stopPropagation();
-//     }
-// });
+var opmopened = false;
+var kbopened = false;
+var sdopened = false;
+
+opmmangabutton.addEventListener('click', function()
+{
+    showpanels1.classList.toggle('manga-list-item-1');
+    if (opmopened == false)
+    {
+        opmopened = true;
+    }
+    else
+    {
+        opmopened = false;
+    }
+
+    if(kbopened == true)
+    {
+        showpanels2.classList.toggle('manga-list-item-2');
+        kbopened = false;
+    }
+    else if (sdopened == true)
+    {
+        showpanels3.classList.toggle('manga-list-item-3');
+        sdopened = false;
+    }
+});
+
+kbmangabutton.addEventListener('click', function()
+{
+    showpanels2.classList.toggle('manga-list-item-2');
+    if (kbopened == false)
+    {
+        kbopened = true;
+    }
+    else
+    {
+        kbopened = false;
+    }
+    
+    if(opmopened == true)
+    {
+        showpanels1.classList.toggle('manga-list-item-1');
+        opmopened = false;
+    }
+    else if (sdopened == true)
+    {
+        showpanels3.classList.toggle('manga-list-item-3');
+        sdopened = false;
+    }
+});
+
+sdmangabutton.addEventListener('click', function()
+{
+    showpanels3.classList.toggle('manga-list-item-3');
+    if (sdopened == false)
+    {
+        sdopened = true;
+    }
+    else
+    {
+        sdopened = false;
+    }
+    
+    if(kbopened == true)
+    {
+        showpanels2.classList.toggle('manga-list-item-2');
+        kbopened = false;
+    }
+    else if (opmopened == true)
+    {
+        showpanels1.classList.toggle('manga-list-item-1');
+        opmopened = false;
+    }
+});
